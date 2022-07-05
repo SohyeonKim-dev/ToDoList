f2b438ac84ff6d9bfb7d3e9d23930f059be9435f
SoHyeon Kim <happythgus89@gmail.com>
2022-07-05 17:45:26 +0900

    UI component design

---
+++ TodoList/Task.swift
@@ -0,0 +1,20 @@
+//
+//  Task.swift
+//  TodoList
+//
+//  Created by 김소현 on 2022/07/05.
+//
+
+import SwiftUI
+
+struct Task: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Task_Previews: PreviewProvider {
+    static var previews: some View {
+        Task()
+    }
+}
--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
 
+    }
+    
+    @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "할 일 등록", message: nil, preferredStyle: .alert)
+        let registerButton = UIAlertAction(title: "등록", style: .default, handler: { _ in
+            debugPrint("\(alert.textFields?[0].text)")
+        })
+        let cancelButton = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alert.addAction(cancelButton)
+        alert.addAction(registerButton)
+        alert.addTextField(configurationHandler: { textField in
+            textField.placeholder = "할 일을 입력해주세요. :)"
+        })
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
